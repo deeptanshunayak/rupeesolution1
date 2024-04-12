53b7d96c54ab23deb75425de57fd6d126afee9bc
deeptanshunayak <deeptanshunayak@gmail.com>
2024-04-12 21:32:09 +0530

    update

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
 import footerb from "../src/images/footer.svg"
+import logo from "../src/images/Rupee solutions.svg"
+import fb from "../src/images/fb.svg"
+import tw from "../src/images/tw.svg"
+import ug from "../src/images/ig.svg"
+import li from "../src/images/link.svg"
+import yt from "../src/images/yt.svg"
 export default function Footer() {
     return (
         <MDBFooter style={{ backgroundColor: '#00063F' }} className='text-center text-lg-start text-muted'>
-            <section className='d-flex justify-content-center justify-content-lg-between p-4'>
+            <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
                 <div className='me-5 d-none d-lg-block'>
 
                 </div>
@@ -35,7 +41,38 @@ export default function Footer() {
                 <MDBContainer className='text-center text-md-start mt-5'>
                     <MDBRow className='mt-3'>
 
-
+                    <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
+                            
+                           <img src={logo}></img>
+                           <br></br>
+                           <br></br>
+                            <p className='text-white'>
+                                <MDBIcon icon="envelope" className="text-white" />
+                                Faridabad,Haryana
+                            </p>
+                            <p className='text-white'>
+                                <MDBIcon icon="phone" className="text-white" /> 
+                                +91 84487 38225
+                            </p>
+                            <p className='text-white'>
+                                <MDBIcon icon="phone" className="text-white" /> 
+                                <p className='text-white'>
+                                <MDBIcon icon="phone" className="text-white" /> 
+                                info@gmail.com
+                            </p>
+                           <img src={fb}></img>
+                           &nbsp;
+                           <img src={tw}></img>
+                           &nbsp;
+                           <img src={ug}></img>
+                           &nbsp;
+                           <img src={li}></img>
+                           &nbsp;
+                           <img src={yt}></img>
+                           &nbsp;
+                            </p>
+                            
+                        </MDBCol>
                         <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
                             <h6 className='text-uppercase fw-bold mb-4 text-white'>Pages</h6>
                             <p className='text-white'>
@@ -90,24 +127,9 @@ export default function Footer() {
                             </p>
                         </MDBCol>
 
-                        <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
-                            <h6 className='text-uppercase fw-bold mb-4 text-white'>Loans</h6>
-                            <p className='text-white'>
-                                <MDBIcon icon="home" className="" />
-                                Education Loan
-                            </p>
-                            <p className='text-white'>
-                                <MDBIcon icon="envelope" className="text-white" />
-                                Gold Loan
-                            </p>
-                            <p className='text-white'>
-                                <MDBIcon icon="phone" className="text-white" /> 
-                                Loan Against Property
-                            </p>
-                            
-                        </MDBCol>
-                        <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4'>
-                        <img src={footerb} className="img-fluid animated1" alt="home img" />
+                        
+                        <MDBCol md="3" lg="4" xl="4" className='mx-auto mb-4'>
+                        <img src={footerb} className="img-fluid" alt="home img" />
                         </MDBCol>
                     </MDBRow>
                 </MDBContainer>
